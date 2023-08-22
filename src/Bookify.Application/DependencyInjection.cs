1a575142bd043cba8ad35774e454f0a8015bbd0b
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-08-22 10:12:07 -0300

    feat: Configure Mediatr injection

---
+++ src/Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Domain.Entities.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
