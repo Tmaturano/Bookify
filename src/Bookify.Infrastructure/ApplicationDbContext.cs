65e44bea1b739e1ae70f98bc842b4260075c4d9f
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-09-19 09:39:56 -0300

    feat: Publishing domain events when saving. (version 1)

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
 ﻿using Bookify.Domain.Entities.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 internal sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
-    {        
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
+    {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -13,5 +17,35 @@ internal sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;        
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        /*
+         Using the ChangeTracker, we grab the entries which implement the Entity class. 
+         Then we grab all the domain events and publish one by one.         
+         */
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            }).ToList();
+
+        foreach (var domainEvent in domainEvents) await _publisher.Publish(domainEvent);
     }
 }
