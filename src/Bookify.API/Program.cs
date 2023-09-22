f6d8aafef4d2a55339b58fbfe83d7fb086c6ef1b
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-09-22 10:49:29 -0300

    setting up dependency injection

--- src/Bookify.API/Program.cs
+++ src/Bookify.API/Program.cs
@@ -1,9 +1,15 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
