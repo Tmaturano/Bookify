4ea39a08d68172e7642d211d90608e082f407800
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-09-22 11:00:33 -0300

    feat: Adding Controllers and endpoints

---
+++ src/Bookify.API/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.API.Controllers.Apartments;
+[Route("api/apartments")]
+[ApiController]
+public class ApartmentsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ApartmentsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchApartments(
+        DateOnly startDate, 
+        DateOnly endDate,
+        CancellationToken cancellationToken)
+    {
+        var query = new SearchApartmentsQuery(startDate, endDate);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+}
---
+++ src/Bookify.API/Controllers/Bookings/ReserveBookingRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.API.Controllers.Bookings;
+
+public sealed record ReserveBookingRequest(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate);
