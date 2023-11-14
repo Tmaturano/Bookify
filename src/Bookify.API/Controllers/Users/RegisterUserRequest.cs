ecff99c6baf793bb8cc461f9af24d5281249cd32
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-11-14 10:34:30 -0300

    feat: Add UsersControllers with register flow

---
+++ src/Bookify.API/Controllers/Users/RegisterUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.API.Controllers.Users;
+
+public sealed record RegisterUserRequest(
+    string Email,
+    string FirstName,
+    string LastName,
+    string Password);
---
+++ src/Bookify.API/Controllers/Users/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Application.Users.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.API.Controllers.Users;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public UsersController(ISender sender) => _sender = sender;
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserRequest request, CancellationToken cancellationToken)
+    {
+        var command = new RegisterUserCommand(
+            request.Email,
+            request.FirstName,
+            request.LastName,
+            request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure) return BadRequest(result.Error);
+
+        return Ok(result.Value);
+    }
+}
